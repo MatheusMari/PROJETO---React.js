cf42c66272b2f8754ee8dae45ea8ad0517aeae32
MatheusMari <rodriguesm.2005@alunos.utfpr.edu.br>
2025-02-06 16:42:22 -0300

    feito a implementação do arquivo de conexão do banco

--- receita-react/src/App.js
+++ receita-react/src/App.js
@@ -6,7 +6,7 @@ import Conteudo from './Conteudo';
 import CategoryList from './CategoryList';
 import RecipeList from './RecipeList';
 import RecipeDetail from './RecipeDetail'; 
-
+import connectDB from '../database/database.js';
 function App() {
   return (
     <Router>
@@ -23,5 +23,21 @@ function App() {
     </Router>
   );
 }
+
+
+
+const app = express();
+const PORT = 3000;
+
+// Conectar ao MongoDB antes de iniciar o servidor
+connectDB();
+
+app.get('/', (req, res) => {
+    res.send("API conectada ao MongoDB!");
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 Servidor rodando em http://localhost:${3000}`);
+});
 
 export default App;
