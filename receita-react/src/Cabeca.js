bbae625f50735b563e2f639a3466212c36ba99f6
MatheusMari <rodriguesm.2005@alunos.utfpr.edu.br>
2025-02-10 16:42:10 -0300

    feito o sistema de linkagem das rotas relacionadas ao botão de login

--- receita-react/src/Cabeca.js
+++ receita-react/src/Cabeca.js
@@ -1,41 +1,39 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'; // Importando o hook useNavigate
 import SettingsIcon from '@mui/icons-material/Settings';
 import './Cabeca.css';
 
 function Cabeca() {
-    return (
+  const navigate = useNavigate(); // Criando o hook de navegação
 
+  // Função para redirecionar para a página de login
+  const handleLoginClick = () => {
+    navigate('/login');
+  };
 
+  return (
+    <div className="Cabeca">
+      <div className="Cabeca-left">
+        <h1 onClick={() => window.location.href = '/login'}>
+          Recipe Haven
+        </h1>
+      </div>
 
-
-        <div className="Cabeca">
-            <div className="Cabeca-left">
-                <h1 onClick={() => window.location.href = '/'}>
-                    Recipe Haven 
-                </h1>
-                
-            </div>
-
-            <div class="box-busca">
-        <div class="search-box">
-            
-            <form method="post" action="#">
-                <input type="text" className="search-box-input" placeholder="Buscar" />
-                <button class="search-box-button"><i class="search-box-icone icon icon-search"></i></button>
-            
-            </form>
-            
+      <div className="box-busca">
+        <div className="search-box">
+          <form method="post" action="#">
+            <input type="text" className="search-box-input" placeholder="Buscar" />
+            <button className="search-box-button"><i className="search-box-icone icon icon-search"></i></button>
+          </form>
         </div>
-    
+      </div>
+
+      <div className="botoesaction">
+        <button className="custom-button" onClick={handleLoginClick}>Login</button>
+        <button className="custom-button">Registrar-se</button>
+      </div>
     </div>
-    <div class="botoesaction">
-    <button class="custom-button">Login</button>
-    <button class="custom-button">Registrar-se</button>
-</div>
-
-        </div>
-        
-    );
+  );
 }
 
 export default Cabeca;
