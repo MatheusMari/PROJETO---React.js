68d561be008c7b6eb45e14a34f92393327b9af52
MatheusMari <rodriguesm.2005@alunos.utfpr.edu.br>
2025-02-10 16:34:48 -0300

    fix: feito adição da barra de pesquisa e botoes de entrar e registro

--- receita-react/src/Cabeca.js
+++ receita-react/src/Cabeca.js
@@ -4,13 +4,37 @@ import './Cabeca.css';
 
 function Cabeca() {
     return (
+
+
+
+
         <div className="Cabeca">
             <div className="Cabeca-left">
                 <h1 onClick={() => window.location.href = '/'}>
-                    Recipe Haven
+                    Recipe Haven 
                 </h1>
+                
             </div>
+
+            <div class="box-busca">
+        <div class="search-box">
+            
+            <form method="post" action="#">
+                <input type="text" className="search-box-input" placeholder="Buscar" />
+                <button class="search-box-button"><i class="search-box-icone icon icon-search"></i></button>
+            
+            </form>
+            
         </div>
+    
+    </div>
+    <div class="botoesaction">
+    <button class="custom-button">Login</button>
+    <button class="custom-button">Registrar-se</button>
+</div>
+
+        </div>
+        
     );
 }
 
