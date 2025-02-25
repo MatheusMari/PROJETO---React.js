46c6cf16c7a8ba96382cd4c9cd6bb33322b8aada
Felipe Brás Coutinho De Oliveira <114227050+FelipeCoutinhoBras@users.noreply.github.com>
2025-02-24 21:53:30 -0300

    Ajustado o código para conectar ao MongoDB usando Mongoose com variáveis de ambiente para a URL de conexão. Vale notar que o Mongoose já gerencia automaticamente o pool de conexões, o que otimiza a comunicação com o banco de dados sem a necessidade de configuração adicional do pool.

--- backend/bd/Servidor.js
+++ backend/bd/Servidor.js
@@ -4,13 +4,13 @@ import dotenv from 'dotenv';
 // Carregar variáveis de ambiente do arquivo .env
 dotenv.config();
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ux9ne.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.c95ad.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 // Conectar ao banco de dados
 mongoose
     .connect(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+        socketTimeoutMS: 45000, // Tempo máximo de inatividade da conexão antes de ser fechada
+        serverSelectionTimeoutMS: 5000, // Timeout para selecionar o servidor
     })
     .then(() => console.log('Conectado ao banco de dados com sucesso!'))
     .catch((error) => console.error('Erro ao conectar ao banco de dados:', error));
